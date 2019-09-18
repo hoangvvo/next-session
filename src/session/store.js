63345c7f2fa10237ad3729c9b994256c72fb303e
Dustin Bachrach <ahdustin@gmail.com>
2019-09-18 11:58:50 -0700

    TypeError with connect-pg-simple Session Store
    Allow express-compatible stores that use Store.call(this, options)
    
    Instead of using es6 classes, this uses util.inherits() like express-session

--- src/session/store.js
+++ src/session/store.js
@@ -1,29 +1,28 @@
-/* eslint-disable class-methods-use-this */
+const util = require('util');
 const EventEmitter = require('events');
 const Session = require('./session');
 const Cookie = require('./cookie');
 
-class Store extends EventEmitter {
-  constructor() {
-    super();
-    EventEmitter.call(this);
-  }
-
-  generate(req, genId, cookieOptions) {
-    req.sessionId = genId;
-    req.session = new Session(req);
-    req.session.cookie = new Cookie(cookieOptions);
-    return req.session;
-  }
-
-  createSession(req, sess) {
-    const thisSess = sess;
-    const { expires } = thisSess.cookie;
-    thisSess.cookie = new Cookie(thisSess.cookie);
-    if (typeof expires === 'string') thisSess.cookie.expires = new Date(expires);
-    req.session = new Session(req, thisSess);
-    return req.session;
-  }
+function Store() {
+  EventEmitter.call(this);
 }
+
+util.inherits(Store, EventEmitter);
+
+Store.prototype.generate = function generate(req, genId, cookieOptions) {
+  req.sessionId = genId;
+  req.session = new Session(req);
+  req.session.cookie = new Cookie(cookieOptions);
+  return req.session;
+};
+
+Store.prototype.createSession = function createSession(req, sess) {
+  const thisSess = sess;
+  const { expires } = thisSess.cookie;
+  thisSess.cookie = new Cookie(thisSess.cookie);
+  if (typeof expires === 'string') thisSess.cookie.expires = new Date(expires);
+  req.session = new Session(req, thisSess);
+  return req.session;
+};
 
 module.exports = Store;
