7223365ba4d41f110cec8f4641a2ec4ae1096c99
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2019-12-08 14:05:59 -0500

    refactor

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-param-reassign */
 const crypto = require('crypto');
 const { promisify } = require('util');
 const MemoryStore = require('./session/memory');
@@ -9,24 +8,17 @@ const { parseToMs } = require('./session/utils');
 
 const genidFn = () => crypto.randomBytes(16).toString('hex');
 
-const hash = (sess) => {
-  const str = JSON.stringify(sess, (key, val) => {
-    if (key === 'cookie') {
-      //  filtered out session.cookie
-      return undefined;
-    }
-    return val;
-  });
-  //  hash
+function hash(sess) {
+  const str = JSON.stringify(sess, (key, val) => (key === 'cookie' ? undefined : val));
   return crypto
     .createHash('sha1')
     .update(str, 'utf8')
     .digest('hex');
-};
+}
 
 let storeReady = true;
 
-const session = (options = {}) => {
+function session(options = {}) {
   const name = options.name || 'sessionId';
   const cookieOptions = options.cookie || {};
   const store = options.store || new MemoryStore();
@@ -34,7 +26,6 @@ const session = (options = {}) => {
   const touchAfter = options.touchAfter ? parseToMs(options.touchAfter) : 0;
   const rollingSession = options.rolling || false;
 
-  //  Notify MemoryStore should not be used in production
   //  eslint-disable-next-line no-console
   if (store instanceof MemoryStore) console.warn('MemoryStore should not be used in production environment.');
 
@@ -44,7 +35,6 @@ const session = (options = {}) => {
   if (store.destroy.length > 2) store.destroy = promisify(store.destroy);
   if (store.touch && store.touch.length > 2) store.touch = promisify(store.touch);
 
-  //  store readiness
   store.on('disconnect', () => {
     storeReady = false;
   });
@@ -53,87 +43,71 @@ const session = (options = {}) => {
   });
 
   return (req, res, next) => {
-    /**
-     * Modify req and res to "inject" the middleware
-     */
-    if (req.session) return next();
+    if (req.session || !storeReady) return next();
 
-    //  check for store readiness before proceeded
-
-    if (!storeReady) return next();
-    //  TODO: add pathname mismatch check
-
-    //  Expose store
     req.sessionStore = store;
-
-    //  Get sessionId cookie from Next.js parsed req.cookies
     req.sessionId = req.cookies[name];
 
-    const getSession = () => {
-      //  Return a session object
+    function getSession() {
       if (!req.sessionId) {
-        //  If no sessionId found in Cookie header, generate one
-        return Promise.resolve(hash(req.sessionStore.generate(req, genid(), cookieOptions)));
+        return Promise.resolve(
+          hash(req.sessionStore.generate(req, genid(), cookieOptions)),
+        );
       }
-      return req.sessionStore.get(req.sessionId)
-        .then((sess) => {
-          if (sess) {
-            return hash(req.sessionStore.createSession(req, sess));
-          }
-          return hash(req.sessionStore.generate(req, genid(), cookieOptions));
-        });
-    };
+      return req.sessionStore.get(req.sessionId).then((sess) => {
+        if (sess) return hash(req.sessionStore.createSession(req, sess));
+        return hash(req.sessionStore.generate(req, genid(), cookieOptions));
+      });
+    }
 
     return getSession().then((hashedsess) => {
       let sessionSaved = false;
       const oldEnd = res.end;
       let ended = false;
-      //  Proxy res.end
+
       res.end = function resEndProxy(...args) {
-        //  If res.end() is called multiple times, do nothing after the first time
         if (ended) {
           return false;
         }
         ended = true;
-        //  save session to store if there are changes (and there is a session)
-        const saveSession = () => {
+
+        function saveSession() {
           if (req.session) {
             if (hash(req.session) !== hashedsess) {
               sessionSaved = true;
               return req.session.save();
             }
-            //  Touch: extend session time despite no modification
             if (req.session.cookie.maxAge && touchAfter >= 0) {
-              const minuteSinceTouched = (
-                req.session.cookie.maxAge
-                  - (req.session.cookie.expires - new Date())
-              );
-              if ((minuteSinceTouched < touchAfter)) {
-                return Promise.resolve();
-              }
+              const minuteSinceTouched = req.session.cookie.maxAge
+                - (req.session.cookie.expires - new Date());
+              if (minuteSinceTouched < touchAfter) return Promise.resolve();
               return req.session.touch();
             }
           }
           return Promise.resolve();
-        };
+        }
 
-        return saveSession()
-          .then(() => {
-            if (
-              (req.cookies[name] !== req.sessionId || sessionSaved || rollingSession)
-                && req.session
-            ) {
-              res.setHeader('Set-Cookie', req.session.cookie.serialize(name, req.sessionId));
-            }
+        return saveSession().then(() => {
+          if (
+            (req.cookies[name] !== req.sessionId
+              || sessionSaved
+              || rollingSession)
+            && req.session
+          ) {
+            res.setHeader(
+              'Set-Cookie',
+              req.session.cookie.serialize(name, req.sessionId),
+            );
+          }
 
-            oldEnd.apply(this, args);
-          });
+          oldEnd.apply(this, args);
+        });
       };
 
       next();
     });
   };
-};
+}
 
 module.exports = session;
 module.exports.Store = Store;
