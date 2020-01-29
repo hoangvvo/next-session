ed9e4e5b4801e1cbfd7b6eb587b263a2fd9e0ce1
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-01-29 18:47:56 -0500

    eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,29 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true,
-        "jest": true,
+  "env": {
+    "es6": true,
+    "node": true,
+    "jest": true,
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parserOptions": {
+    "ecmaFeatures": {
+        "jsx": true
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended"
-    ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-    }
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "rules": {
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off"
+  }
 };
