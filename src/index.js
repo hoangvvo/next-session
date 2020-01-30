8ce09d4fa23cc809caafac928d4731c5c4dc559e
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-01-29 23:11:30 -0500

    No default export

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
-/* eslint-disable no-param-reassign */
-import ConnectMiddleware from './connect';
-
 export { default as withSession } from './withSession';
-export { ConnectMiddleware as connect };
+export { default as session } from './connect';
 export { applySession } from './core';
-export { applySession as useSession } from './core';
 export { default as MemoryStore } from './store/memory';
 export { default as Store } from './store';
-export default ConnectMiddleware;
