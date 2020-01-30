edcc8433a488803759a01946536a6c837a0264bd
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-01-29 23:59:58 -0500

    Include promisifyStore

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ export { default as session } from './connect';
 export { applySession } from './core';
 export { default as MemoryStore } from './store/memory';
 export { default as Store } from './store';
+export { promisifyStore } from './compat';
