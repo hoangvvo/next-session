bb8419292a1b654df7da6bbd14aacc5ac62b9a05
Hoang Vo <40987398+hoangvvo@users.noreply.github.com>
2020-01-29 19:09:23 -0500

    Update jest config

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,11 @@
 module.exports = {
+  rootDir: "integration",
   testEnvironment: 'node',
   collectCoverageFrom: ['lib/**/*'],
   testMatch: ['**/*.test.js'],
   verbose: true,
+  transform: {
+    "^.+\\.jsx?$": "babel-jest"
+  },
+  bail: true,
 };
