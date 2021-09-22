4a1c720627034912bc68f15f8d291d5e8a94da53
Hoang Vo <dev+g@hvvo.dev>
2021-09-22 17:35:49 +0700

    Make MemoryStore.store assignment in constructor

--- test/import-test/index.cjs
+++ test/import-test/index.cjs
@@ -1,7 +1,9 @@
 const assert = require("assert");
-const session = require("next-session");
+const session = require("next-session").default;
 const { expressSession, promisifyStore } = require("next-session/lib/compat");
 
 assert(session);
 assert(expressSession);
 assert(promisifyStore);
+
+session();
