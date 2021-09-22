49fc8b28967a7775b5e9db5dba0e37f26637cf89
Hoang Vo <dev+g@hvvo.dev>
2021-09-22 18:19:20 +0700

    Fix build for ES2020 features

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,22 @@
-export default process.env.BUILD_MODULE === "commonjs"
-  ? {
-      plugins: [
-        ["babel-plugin-add-import-extension", { extension: "cjs" }],
-        "@babel/plugin-transform-modules-commonjs",
-      ],
-      presets: ["@babel/preset-typescript"],
-    }
-  : {
-      plugins: [["babel-plugin-add-import-extension", { extension: "js" }]],
-      presets: ["@babel/preset-typescript"],
-    };
+const commonjs = process.env.BUILD_MODULE === "commonjs";
+
+export default {
+  plugins: [
+    [
+      "babel-plugin-add-import-extension",
+      { extension: commonjs ? "cjs" : "js" },
+    ],
+  ],
+  presets: [
+    "@babel/preset-typescript",
+    [
+      "@babel/preset-env",
+      {
+        targets: {
+          node: "12",
+        },
+        modules: commonjs ? "commonjs" : false,
+      },
+    ],
+  ],
+};
