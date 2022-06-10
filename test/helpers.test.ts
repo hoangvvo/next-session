c0acfbe8aca6658020218c4a32c065895ac392be
Hoang <40987398+hoangvvo@users.noreply.github.com>
2022-06-11 03:34:11 +0700

    Update helpers.test.ts

--- test/helpers.test.ts
+++ test/helpers.test.ts
@@ -1,29 +1,51 @@
-import { isNew, isTouched, isDestroyed } from "../src/helpers";
-import session from "../src/session";
+import { isDestroyed, isNew, isTouched } from "../src/helpers";
+import {
+  isDestroyed as isDestroyedSymbol, isNew as isNewSymbol,
+  isTouched as isTouchedSymbol
+} from "../src/symbol";
 
 describe("isNew()", () => {
-  test("if returns undefined", () => {
-    expect(isNew(session)).toBe(undefined);
+  test("returns true if session is new", () => {
+    const sess = {
+      [isNewSymbol]: true
+    }
+    expect(isNew(sess)).toBe(true);
   });
-  test("if it returns a boolean", () => {
-    expect(typeof isNew(session) === "boolean").toBeFalsy();
+  test("returns false if session is not new", () => {
+    // an existing session does not have this property
+    // see line 70 src/session.ts
+    const sess = {}
+    expect(isNew(sess)).toBe(false);
   });
 });
 
 describe("isTouched()", () => {
-  test("if returns undefined", () => {
-    expect(isTouched(session)).toBe(undefined);
+  test("returns true if session is touched", () => {
+    const sess = {
+      [isTouchedSymbol]: true
+    }
+    expect(isTouched(sess)).toBe(true);
   });
-  test("if it returns a boolean", () => {
-    expect(typeof isTouched(session) === "boolean").toBeFalsy();
+  test("returns false if session is untouched", () => {
+    // an untouched session does not have this property
+    const sess = {}
+    expect(isTouched(sess)).toBe(false);
   });
 });
 
 describe("isDestroyed()", () => {
-  test("if returns undefined", () => {
-    expect(isDestroyed(session)).toBe(undefined);
+  test("returns true if session is destroyed", () => {
+    const sess = {
+      [isDestroyedSymbol]: true
+    }
+    expect(isDestroyed(sess)).toBe(true);
   });
-  test("if it returns a boolean", () => {
-    expect(typeof isDestroyed(session) === "boolean").toBeFalsy();
+  test("returns false if session is destroyed", () => {
+    // an undestroyed session does not have this property
+    const sess = {}
+    expect(isDestroyed(sess)).toBe(false);
+    // when a session is destroyed, req.session will be unset
+    // if a user passes in req.session, we should return false too
+    expect(isDestroyed(undefined)).toBe(false);
   });
 });
